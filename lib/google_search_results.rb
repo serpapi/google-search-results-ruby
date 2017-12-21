74a8b8aee9d4ef3a618a604fc4c0370e4ebef82b
hartator <hartator@gmail.com>
2017-12-21 16:18:38 -0600

    Wrap SERP API inside a Ruby class

---
+++ lib/google_search_results.rb
@@ -0,0 +1,37 @@
+# encoding: UTF-8
+
+require 'open-uri'
+require 'json'
+require_relative 'google_search_results/hash'
+
+class GoogleSearchResults
+
+  VERSION = "0.0.1"
+  BACKEND = "serpapi.com"
+
+  attr_accessor :params
+
+  def initialize params
+    @params = params
+  end
+
+  def construct_url
+    @params[:source] = "ruby"
+    URI::HTTPS.build(host: BACKEND, path: '/search', query: URI.encode_www_form(@params))
+  end
+
+  def get_html
+    @params[:output] = "html"
+    open(construct_url).read
+  end
+
+  def get_json
+    @params[:output] = "json"
+    open(construct_url).read
+  end
+
+  def get_hash
+    JSON.load(get_json).symbolize_all_keys
+  end
+
+end 
