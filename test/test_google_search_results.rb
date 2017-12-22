172fb04727bf05828d2dd98564be606235cfaace
hartator <hartator@gmail.com>
2017-12-22 14:58:43 -0600

    Test correctly setup of API key

--- test/test_google_search_results.rb
+++ test/test_google_search_results.rb
@@ -6,11 +6,22 @@ require 'google_search_results'
 class GoogleSearchResultsTest < Minitest::Test
 
   def setup
+    GoogleSearchResults.serp_api_key = nil
     @query = GoogleSearchResults.new(q: "Coffee", location: "Portland")
   end
 
   def test_construct_url
     assert_equal "https://serpapi.com/search?q=Coffee&location=Portland&source=ruby", @query.construct_url.to_s
+  end
+
+  def test_set_serp_api_key_on_class
+    GoogleSearchResults.serp_api_key = "secret_key_from_class"
+    assert_includes @query.construct_url.to_s, "secret_key_from_class"
+  end
+
+  def test_set_serp_api_key_with_params
+    @query.params[:serp_api_key] = "secret_key_from_params"
+    assert_includes @query.construct_url.to_s, "secret_key_from_params"
   end
 
   def test_html_output
