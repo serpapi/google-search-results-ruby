432251a3e326688ae3040a009ca7e0fad9350c0e
jvmvik <jvmvik@gmail.com>
2019-03-08 06:59:07 -0600

    stub response when running on TravisCI

--- test/search_archive_api_spec.rb
+++ test/search_archive_api_spec.rb
@@ -7,8 +7,20 @@ describe "Search Archive API" do
   end
   
   it 'example' do
+    # mock 
+    search_response_mock =  %q({
+        "search_metadata": {
+          "id": "cdbdbdebeabab1beb"
+        }
+      })
+
     # run a search
     gsr = GoogleSearchResults.new(q: "Coffee", location: "Portland")
+
+    if GoogleSearchResults.serp_api_key.nil?
+      puts "mock response"
+      allow(gsr).to receive(:get_results) { search_response_mock }
+    end
     original_search = gsr.get_hash
 
     search_id = original_search[:search_metadata][:id]
@@ -16,6 +28,9 @@ describe "Search Archive API" do
 
     # retrieve search from archive
     gsr = GoogleSearchResults.new
+    if GoogleSearchResults.serp_api_key.nil?
+      allow(gsr).to receive(:get_results) { search_response_mock }
+    end
     archive_search = gsr.get_search_archive(search_id)
   
     expect(archive_search).to eq(original_search)
