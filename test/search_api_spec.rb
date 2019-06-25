dc6e26982dc98809126c09d29b1f28ac0a6f20e4
jvmvik <jvmvik@gmail.com>
2019-06-25 08:10:38 -0500

    add test for baidu and bing

--- test/search_api_spec.rb
+++ test/search_api_spec.rb
@@ -1,6 +1,6 @@
 require_relative '../lib/google_search_results'
 
-describe "Search API" do
+describe "Google Search API Google" do
 
   before(:all) do
     GoogleSearchResults.serp_api_key = ENV['SERPAPI_KEY']
@@ -22,6 +22,60 @@ describe "Search API" do
     expect(@client.get_html).to match /coffee/
   end
 
+end
+
+describe "Bing Search API" do
+
+  before(:all) do
+    GoogleSearchResults.serp_api_key = ENV['SERPAPI_KEY']
+    @client = GoogleSearchResults.new(q: "Coffee", location: "Portland", engine: "Bing")
+  end
+
+  it 'get_hash' do
+    hash = @client.get_hash
+    expect(hash[:search_metadata][:status]).to eq('Success')
+    expect(hash[:search_metadata][:bing_url]).to match(/www.bing.com/)
+    expect(hash[:organic_results].size).to be >5
+    expect(hash[:organic_results].to_s).to match /coffee/
+    expect(hash[:ads].size).to be >5
+  end
+
+  it 'get_json' do
+    json = @client.get_json
+    expect(json.size).to be > 9000
+    expect(json).to match /coffee/
+  end
+
+  it 'get_html' do
+    expect(@client.get_html).to match /coffee/
+  end
+end
+
+describe "Baidu Search API" do
+
+  before(:all) do
+    GoogleSearchResults.serp_api_key = ENV['SERPAPI_KEY']
+    @client = GoogleSearchResults.new(q: "Coffee", location: "Portland", engine: "Baidu")
+  end
+
+  it 'get_hash' do
+    hash = @client.get_hash
+    expect(hash[:search_metadata][:status]).to eq('Success')
+    expect(hash[:search_metadata][:baidu_url]).to match(/www.baidu.com/)
+    expect(hash[:organic_results].size).to be >7
+    expect(hash[:organic_results].to_s).to match /coffee/
+    expect(hash[:pagination]).not_to be_empty
+  end
+
+  it 'get_json' do
+    json = @client.get_json
+    expect(json.size).to be > 9000
+    expect(json).to match /coffee/
+  end
+
+  it 'get_html' do
+    expect(@client.get_html).to match /coffee/
+  end
 end
 
 describe 'low level' do
